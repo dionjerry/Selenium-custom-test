bb5016572d1063b3affecb51d4f80a0bafa8eaee
dionjerry <dionjerry442@gmail.com>
2023-04-11 23:27:04 +0100

    welcome to testing
    you can simply test any website with this.
    code is still in development.

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,13 @@ import org.testng.annotations.Test;
 
 public class Main {
 
-
+    //************************************************************************************
+    //Variable TExt
     String ActualTitle = "Your Title";
-    String   Website = "http://cardtonic.com"; //Enter Website Name
-    String WebDrive = "C:\\\\Users\\\\DON JERRY\\\\Documents\\\\softwares\\\\cchromedriver.exe"; //Web driver location chromium is used
+    String   Website = "http://YourWebsiteAddress"; //Enter Website Name
+    String WebDrive = "Your Webdriver Location"; //Web driver location chromium is used
     int ScrollPixel = 1000 ; //number of pixel to scroll through your website
+    //************************************************************************************
     ChromeOptions options = new ChromeOptions();
     String Title;
     public WebDriver driver;
@@ -50,7 +52,7 @@ public class Main {
     }
 
     @Test(priority = 1)
-    void Responsive() // TEST WEBSITE RESPONSIVENESS
+    void ResponsiveScroll() throws InterruptedException // TEST WEBSITE RESPONSIVENESS
     {
 
         JavascriptExecutor js = (JavascriptExecutor)driver;
@@ -64,7 +66,41 @@ public class Main {
         for(int i =0 ; i > -(ScrollPixel) ; i --) {
            // System.out.println(i); //code test
             js.executeScript("window.scrollBy(0, "+i+")"); }
+        Thread.sleep(3000);
 
+
+
+    }
+
+    @Test(priority = 2 )
+    void VisitPages()
+    {
+        WebElement bodyText = driver.findElement(By.cssSelector("body")); //locates all text in website
+       String BodilyText = bodyText.getText(); //grabs all the text in the website
+        System.out.println(BodilyText);
+        String[] BodyTextArray = BodilyText.split("\n");
+        System.out.println(BodyTextArray.length); // displays the texts divided into lines.
+        for(String Clicker : BodyTextArray )
+        {
+
+            try
+            {
+
+                System.out.println("visiting " +Clicker + " Relax");
+
+                driver.findElement(By.linkText(Clicker)).click(); //locate each text and click
+                Thread.sleep(3000);
+                driver.navigate().back();
+                Thread.sleep(3000);
+            }
+            catch (NoSuchElementException e)
+            {
+                    //this is empty for catching unClickable links
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e); //this is for the thread waiting..
+            }
+
+        }
 
     }
 
